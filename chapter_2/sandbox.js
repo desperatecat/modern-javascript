c0c6510042b7f1568cdf44bb5dd0476d31d2f6b1
desperatecat <sijialucas@gmail.com>
2020-08-03 04:26:20 +0800

    todo list

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,3 +1,27 @@
-alert('hello world!');
-console.log(1);
-console.log(2);
+// let age = 25;
+// let year = 2020;
+
+// console.log(age, year);
+
+// age = 30;
+
+// console.log(age);
+
+// let score = '100';
+// score = Number(score);
+
+
+// const great = () => 'hello world';
+// console.log(great());
+
+// const para = document.querySelector('div.error');
+
+
+
+const paras = document.querySelectorAll('.error');
+
+paras.forEach(para => {
+    console.log(para);
+});
+
+console.log(paras);
